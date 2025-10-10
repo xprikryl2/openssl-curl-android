a33e40fca4d28c682924b186e9f9795f03e7f892
oprikryl <ondrej.prikryl@ahead-itec.com>
2025-10-10 09:44:19 +0200

    global flags modified

--- my-build.sh
+++ my-build.sh
@@ -4,7 +4,7 @@ export NDK=$HOME/Library/Android/sdk/ndk/27.2.12479018
 export HOST_TAG=darwin-x86_64
 export MIN_SDK_VERSION=23
 
-export CFLAGS="-Os -flto -fvisibility=hidden"
+export CFLAGS="-Os -fvisibility=hidden"
 export LDFLAGS="-Wl,-Bsymbolic,-z,max-page-size=16384"
 
 ./build.sh
